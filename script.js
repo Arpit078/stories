af541e90c7ab2effe7bb76bd62f029b3cb8b3b11
Arpit078 <verma.arpit078@.com>
2022-12-30 02:12:39 +0530

    loader added

--- script.js
+++ script.js
@@ -2,15 +2,49 @@ const container = document.getElementById("container")
 const heading = document.getElementById("title")
 const content = document.getElementById("content")
 const toggle = document.getElementById("toggle")
+const loader = document.getElementById("loader")
 const url = "https://stories-api.onrender.com/"
+// const url = "http://localhost:5000/"
+
+const pageArr = [
+    "n\n2\n\n",
+    "n\n3\n\n",
+    "n\n4\n\n",
+    "n\n5\n\n",
+    "n\n6\n\n",
+    "n\n7\n\n",
+]
+positions = []
+// console.log(pageArr["n\n2\n\n"])
+
+
+const displayLoading = () => {
+    container.style.display = 'none';
+    loader.style.display = 'block';
+
+};
+
+const hideLoading = () => {
+    loader.style.display = 'none';
+    container.style.display = 'block';
+};
+
+
+
+
 
 async function getData(link){
+    displayLoading()
     const res = await fetch(link)
     const data = await res.json()
+    // for(i=0;i<pageArr.length;i++){
+    //     pos = data.content.search(pageArr[i])
+    //     positions[i] = pos
+
+    // }
+    // console.log(positions)
     return data
 }
-
-
 
 toggle.addEventListener("click", function(){
     if(toggle.checked){
@@ -28,7 +62,9 @@ toggle.addEventListener("click", function(){
   });
 
 getData(url).then((res)=>{
+    hideLoading()
     heading.innerHTML = res.heading
     content.innerHTML = res.content
+
 
 })
